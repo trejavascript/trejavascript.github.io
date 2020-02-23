0223f9806d1a72e21b26acc519935973af46b0c7
trejavascript <trejavascript@gmail.com>
2020-02-22 22:17:13 -0500

    update main branch

--- common.js
+++ common.js
@@ -3,29 +3,29 @@
 //
 
 var Loader = {
-    images: {}
+  images: {}
 };
 
-Loader.loadImage = function (key, src) {
-    var img = new Image();
+Loader.loadImage = function(key, src) {
+  var img = new Image();
 
-    var d = new Promise(function (resolve, reject) {
-        img.onload = function () {
-            this.images[key] = img;
-            resolve(img);
-        }.bind(this);
+  var d = new Promise(function(resolve, reject) {
+    img.onload = function() {
+      this.images[key] = img;
+      resolve(img);
+    }.bind(this);
 
-        img.onerror = function () {
-            reject('Could not load image: ' + src);
-        };
-    }.bind(this));
+    img.onerror = function() {
+      reject('Could not load image: ' + src);
+    };
+  }.bind(this));
 
-    img.src = src;
-    return d;
+  img.src = src;
+  return d;
 };
 
-Loader.getImage = function (key) {
-    return (key in this.images) ? this.images[key] : null;
+Loader.getImage = function(key) {
+  return (key in this.images) ? this.images[key] : null;
 };
 
 //
@@ -41,36 +41,36 @@ Keyboard.DOWN = 40;
 
 Keyboard._keys = {};
 
-Keyboard.listenForEvents = function (keys) {
-    window.addEventListener('keydown', this._onKeyDown.bind(this));
-    window.addEventListener('keyup', this._onKeyUp.bind(this));
+Keyboard.listenForEvents = function(keys) {
+  window.addEventListener('keydown', this._onKeyDown.bind(this));
+  window.addEventListener('keyup', this._onKeyUp.bind(this));
 
-    keys.forEach(function (key) {
-        this._keys[key] = false;
-    }.bind(this));
+  keys.forEach(function(key) {
+    this._keys[key] = false;
+  }.bind(this));
 }
 
-Keyboard._onKeyDown = function (event) {
-    var keyCode = event.keyCode;
-    if (keyCode in this._keys) {
-        event.preventDefault();
-        this._keys[keyCode] = true;
-    }
+Keyboard._onKeyDown = function(event) {
+  var keyCode = event.keyCode;
+  if (keyCode in this._keys) {
+    event.preventDefault();
+    this._keys[keyCode] = true;
+  }
 };
 
-Keyboard._onKeyUp = function (event) {
-    var keyCode = event.keyCode;
-    if (keyCode in this._keys) {
-        event.preventDefault();
-        this._keys[keyCode] = false;
-    }
+Keyboard._onKeyUp = function(event) {
+  var keyCode = event.keyCode;
+  if (keyCode in this._keys) {
+    event.preventDefault();
+    this._keys[keyCode] = false;
+  }
 };
 
-Keyboard.isDown = function (keyCode) {
-    if (!keyCode in this._keys) {
-        throw new Error('Keycode ' + keyCode + ' is not being listened to');
-    }
-    return this._keys[keyCode];
+Keyboard.isDown = function(keyCode) {
+  if (!keyCode in this._keys) {
+    throw new Error('Keycode ' + keyCode + ' is not being listened to');
+  }
+  return this._keys[keyCode];
 };
 
 //
@@ -79,42 +79,42 @@ Keyboard.isDown = function (keyCode) {
 
 var Game = {};
 
-Game.run = function (context) {
-    this.ctx = context;
-    this._previousElapsed = 0;
+Game.run = function(context) {
+  this.ctx = context;
+  this._previousElapsed = 0;
 
-    var p = this.load();
-    Promise.all(p).then(function (loaded) {
-        this.init();
-        window.requestAnimationFrame(this.tick);
-    }.bind(this));
+  var p = this.load();
+  Promise.all(p).then(function(loaded) {
+    this.init();
+    window.requestAnimationFrame(this.tick);
+  }.bind(this));
 };
 
-Game.tick = function (elapsed) {
-    window.requestAnimationFrame(this.tick);
+Game.tick = function(elapsed) {
+  window.requestAnimationFrame(this.tick);
 
-    // clear previous frame
-    this.ctx.clearRect(0, 0, 512, 512);
+  // clear previous frame
+  this.ctx.clearRect(0, 0, 512, 512);
 
-    // compute delta time in seconds -- also cap it
-    var delta = (elapsed - this._previousElapsed) / 1000.0;
-    delta = Math.min(delta, 0.25); // maximum delta of 250 ms
-    this._previousElapsed = elapsed;
+  // compute delta time in seconds -- also cap it
+  var delta = (elapsed - this._previousElapsed) / 1000.0;
+  delta = Math.min(delta, 0.25); // maximum delta of 250 ms
+  this._previousElapsed = elapsed;
 
-    this.update(delta);
-    this.render();
+  this.update(delta);
+  this.render();
 }.bind(Game);
 
 // override these methods to create the demo
-Game.init = function () {};
-Game.update = function (delta) {};
-Game.render = function () {};
+Game.init = function() {};
+Game.update = function(delta) {};
+Game.render = function() {};
 
 //
 // start up function
 //
 
-window.onload = function () {
-    var context = document.getElementById('demo').getContext('2d');
-    Game.run(context);
+window.onload = function() {
+  var context = document.getElementById('demo').getContext('2d');
+  Game.run(context);
 };
